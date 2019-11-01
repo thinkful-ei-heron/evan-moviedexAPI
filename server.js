6b88efac61320716d47b2731d358ae0159ae720c
Evan Vogts <evanjvogts@gmail.com>
2019-11-01 14:05:57 -0500

    ready for deployment

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const helmet = require('helmet');
 const Movies = require('./movies-data-small.json');
 
 const app = express();
-
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+app.use(morgan(morganSetting));
 app.use(helmet());
 app.use(cors());
 
@@ -15,7 +15,6 @@ app.use(cors());
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
   const authToken = req.get('Authorization');
-  console.log('validate bearer token middleware');
   if (!authToken || authToken.split(' ')[1] !== apiToken) {
     return res.status(401).json({ error: 'Unauthorized request' });
   }
@@ -58,8 +57,16 @@ app.get('/movies', (req, res) => {
     .json(results);
 });
 
-const PORT = 8000;
-
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }};
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
 });
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {});
